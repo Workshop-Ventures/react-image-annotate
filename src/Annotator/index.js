b95489db55911152b92a8fbc748bc9dfc672b134
Chad <chad@simplisafe.com>
2021-04-30 08:15:53 -0400

    Added tool comment

--- src/Annotator/index.js
+++ src/Annotator/index.js
@@ -63,6 +63,8 @@ export const Annotator = ({
   showTags = getFromLocalStorage("showTags", true),
   enabledTools = [
     "select",
+    // "pan",
+    // "zoom",
     "create-point",
     "create-box",
     "create-polygon",
